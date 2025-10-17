0633d1f60cd4ba670e948658a82de554e9663c71
Ebi-Electronics <ebi.sound.2025@gmail.com>
2025-10-17 15:21:26 +0200

    Add files via upload

---
+++ pelicanconf.py
@@ -0,0 +1,45 @@
+AUTHOR = 'RDash'
+SITENAME = 'Ebi Electronics'
+SITEURL = "https://ebi-electronics.github.io/"
+SITESUBTITLE = 'Another blog about electronics. Now with sea taste!'
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'EN'
+
+THEME = 'themes/editoral'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Ebi Sound", "https://www.youtube.com/"),
+    ("Ebi Electronics", "https://www.youtube.com/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+CONTACTS = [
+    ("Twitter", "twitter", "https://twitter.com/theanalogfox"),
+    ("Facebook", "facebook-f", "https://facebook.com/theanalogfox"),
+    ("Instagram", "instagram", "https://www.instagram.com/theanalogfox/"),
+    ("Email", "envelope", "info@theanalogfox.com"),
+]
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
